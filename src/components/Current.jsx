32c3953c056a996f5894cfa14d42ca168e6ff324
XX <fallwall19@gmail.com>
2020-09-15 18:48:07 -0700

    font trial

--- src/components/Current.jsx
+++ src/components/Current.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
+const fontStyle = {
+  color: 'blue',
+  fontFamily: 'Lato',
+  fontStyle: 'normal',
+  fontWeight: 'normal',
+  src: `url(http://fonts.gstatic.com/s/lato/v17/S6uyw4BMUTPHvxk6XweuBCY.ttf) format('truetype')`
+}
+
+
 export default function Current(props) {
   return (
     <div>
       {props.font && <div><h1>{props.font.family}</h1></div>}
+      {props.font && <div><p>{props.font.files.regular}</p></div>}
+      <p style={fontStyle}>TYPED RESULT HERE</p>
     </div>
   )
 }
