c23c04dba3806562e76d8c13f78d7e158741305f
XX <fallwall19@gmail.com>
2020-09-16 10:11:11 -0700

    font selectino in place

--- src/components/Current.jsx
+++ src/components/Current.jsx
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import WebFont from 'webfontloader';
 
-WebFont.load({
-  google: {
-    families: ['Ranchers', 'sans-serif']
-  }
-});
 
-const fontStyle = {
-  color: 'blue',
-  fontFamily: 'Ranchers'
-}
 
 
 export default function Current(props) {
+  const [input, setInput] = useState("");
+  const font = props.font && props.font.family;
+
+  WebFont.load({
+    google: {
+      families: [`${font}`, 'sans-serif']
+    }
+  });
+  
+  const fontStyle = {
+    color: 'blue',
+    fontFamily: `${font}`
+  }
   return (
     <div>
       {props.font && <div><h1>{props.font.family}</h1></div>}
+      <input
+        type="text"
+        placeholder="Roe is a cat..."
+        onChange={e => setInput(e.target.value)}
+      />
       {props.font && <div><p>{props.font.files.regular}</p></div>}
       <p style={fontStyle}>TYPED RESULT HERE</p>
+      <p style={fontStyle}>{input}</p>
 
     </div>
   )
